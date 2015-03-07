30a95ef5db365b96e4087986edb1b44542149ed1
André Valle <andrevvalle@icloud.com>
2015-03-07 12:51:18 -0300

    adjustment function close js

--- Todo/Js/js/function.js
+++ Todo/Js/js/function.js
@@ -16,26 +16,30 @@ btRegistrer.addEventListener('click', function(evt){
 		
 	}else{
 
-		listTask.innerHTML += '<li id="taskItem"><h4>'+task.value+'<div id="iconClose">x</div></h4><p>'+description.value+'</p></li>';
+		listTask.innerHTML += '<li id="taskItem"><h4>'+task.value+'<div class="iconClose">x</div></h4><p>'+description.value+'</p></li>';
 
 		// rmv campo error
 		task.classList.remove('error');
 		description.classList.remove('error');
 	};
 
-
 	// //remove
 	task.value = '';
 	description.value = '';
 
-	var taskItem = document.getElementById('taskItem');
-	var iconClose = document.querySelector('#iconClose')
+	closeTask();
 
-	
-	
+});
 
+function closeTask(){
+	var elements = document.querySelectorAll('.iconClose');
 
+	for(var i = 0, len = elements.length; i < len; i++) {
+	    elements[i].onclick = function () {
+			this.closest('li').remove();
+	    }
+	}
+};
 
-});	
 
 
